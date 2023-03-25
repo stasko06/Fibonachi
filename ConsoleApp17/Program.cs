088422597317f64095c19b7176857c1e80437b59
shket <shket@ACERNITRO>
2023-03-25 11:19:00 +0200

    Add project files.

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,14 @@
+﻿static int Fibonachi (int n)
+{
+    int[] a = new int[n];
+    a[0] = 0;
+    a[1] = 1;
+    for (int i = 2; i<a.Length; i++)
+    {
+        a[i] = a[i-1] + a[i-2];
+
+    }
+    return a[n];
+}
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine(Fibonachi(n));
